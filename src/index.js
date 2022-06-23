113c50483fd063c4ade0895cdf8226dd9f27473f
SeanMabli <smdrone1@gmail.com>
2022-06-23 12:41:49 -0400

    work on navbar architecture and style

--- src/index.js
+++ src/index.js
@@ -1,55 +1,92 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
-import { Button, Menu, MenuItem } from '@mui/material';
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import { Button, Menu, MenuItem } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 import "./firebase";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#547c94",
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <React.StrictMode>
-      <p>smilemass</p>
-      <Dropdown />
-    </React.StrictMode>
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <Dropdown
+          title="About"
+          options={[
+            "Our Mission",
+            "The Staff",
+            "Testimonials",
+            "Accomplishments",
+            "Sudbury Accesible Playgrounds",
+            "F. A. Q.",
+            "Contact",
+          ]}
+        />
+        <Dropdown
+          title="Our Programs"
+          options={[
+            "Club SMILE Mass",
+            "Local Beach Wheelchair Locations",
+            "Beach House",
+            "Community wihtin a Community",
+            "Equipment Loaner Program",
+          ]}
+        />
+        <Dropdown
+          title="Get Involved"
+          options={["Running Team", "Volunteer"]}
+        />
+        <Dropdown
+          title="News & Events"
+          options={["Events", "In The News", "Newsletters", "SMILE Blog"]}
+        />
+        <Button>Resources</Button>
+        <Dropdown
+          title="Support"
+          options={["Join the Coffee Club", "Donate"]}
+          bold={true}
+        />
+      </ThemeProvider>
+    </React.Fragment>
   </>
 );
 
+function Dropdown(props) {
+  var buttonType = props.bold ? "contained" : "text";
+  var buttonStyle = {
+    marginLeft: "10px",
+    marginRight: "10px",
+    paddingBottom: "4px",
+  };
 
-function Dropdown() {
   return (
     <>
-    <PopupState variant="popover" popupId="demo-popup-menu" className="navbardropdown">
-      {(popupState) => (
-        <React.Fragment>
-          <Button variant="contained" {...bindTrigger(popupState)}>
-            About
-          </Button>
-          <Menu {...bindMenu(popupState)}>
-            <MenuItem onClick={popupState.close}>Our Mission</MenuItem>
-            <MenuItem onClick={popupState.close}>The Staff</MenuItem>
-            <MenuItem onClick={popupState.close}>Testimonials</MenuItem>
-            <MenuItem onClick={popupState.close}>Accompishments</MenuItem>
-            <MenuItem onClick={popupState.close}>Sudbury Accessible Playgrounds</MenuItem>
-            <MenuItem onClick={popupState.close}>F. A. Q.</MenuItem>
-            <MenuItem onClick={popupState.close}>Contact</MenuItem>
-          </Menu>
-        </React.Fragment>
-      )}
-    </PopupState>
-    <PopupState variant="popover" popupId="demo-popup-menu" className="navbardropdown">
-      {(popupState) => (
-        <React.Fragment>
-          <Button variant="contained" {...bindTrigger(popupState)}>
-            Dashboard
-          </Button>
-          <Menu {...bindMenu(popupState)}>
-            <MenuItem onClick={popupState.close}>Profile</MenuItem>
-            <MenuItem onClick={popupState.close}>My account</MenuItem>
-            <MenuItem onClick={popupState.close}>Logout</MenuItem>
-          </Menu>
-        </React.Fragment>
-      )}
-    </PopupState>
+      <PopupState variant="popover" popupId="demo-popup-menu">
+        {(popupState) => (
+          <React.Fragment>
+            <Button
+              {...bindTrigger(popupState)}
+              style={buttonStyle}
+              variant={buttonType}
+            >
+              {props.title}
+            </Button>
+            <Menu {...bindMenu(popupState)}>
+              {props.options.map((option) => (
+                <MenuItem onClick={popupState.close}>{option}</MenuItem>
+              ))}
+            </Menu>
+          </React.Fragment>
+        )}
+      </PopupState>
     </>
   );
 }
