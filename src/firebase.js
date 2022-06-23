52d85aaeda6ba3623805c35fa87d8589bf88e528
SeanMabli <smdrone1@gmail.com>
2022-06-22 20:27:28 -0400

    init

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvlwNhLDUO_IuFFbl0wa4QTJCKWsszWcU",
+  authDomain: "smilemass-6902d.firebaseapp.com",
+  projectId: "smilemass-6902d",
+  storageBucket: "smilemass-6902d.appspot.com",
+  messagingSenderId: "592962324011",
+  appId: "1:592962324011:web:1a4eb05c087c6fcc730c6e",
+  measurementId: "G-HZ396JK36Y"
+};
+
+export const app = initializeApp(firebaseConfig);
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./firebase";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <React.StrictMode>
+      <p>smilemass</p>
+    </React.StrictMode>
+  </>
+);
