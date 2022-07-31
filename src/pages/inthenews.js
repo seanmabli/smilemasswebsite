522de980827d24f5e3c5e8f67b02459d89498583
SeanMabli <smdrone1@gmail.com>
2022-07-31 18:38:14 -0400

    in the news page v2

--- src/pages/inthenews.js
+++ src/pages/inthenews.js
@@ -1,28 +1,14 @@
 import "./inthenews.css";
-import { NewsButton } from "../components/mui";
 import { useState, useEffect } from "react";
-import { db, storage } from "../firebase/firebase";
+import { db } from "../firebase/firebase";
 import { collection, getDocs } from "firebase/firestore";
-import { ref, getDownloadURL } from "firebase/storage";
 
 export default function InTheNews() {
   const [newsItems, setNewsItems] = useState([]);
-  const [newsItemImages, setNewsItemImages] = useState([]);
-
-  const storageRef = ref(storage, "inthenews");
-
   useEffect(() => {
     const getNewsItems = async () => {
       const data = await getDocs(collection(db, "inthenews"));
       setNewsItems(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      for (let i = 0; i < data.docs.length; i++) {
-        const imageRef = ref(storageRef, data.docs[i].data().imageurl);
-        const imageUrl = await getDownloadURL(imageRef);
-        setNewsItemImages((newsItemImages) => [
-          ...newsItemImages,
-          { url: imageUrl, published: data.docs[i].data().published },
-        ]);
-      }
     };
     getNewsItems();
   }, []);
@@ -30,17 +16,7 @@ export default function InTheNews() {
   newsItems.sort(function (first, second) {
     return second.published["seconds"] - first.published["seconds"];
   });
-
-  newsItemImages.sort(function (first, second) {
-    return second.published["seconds"] - first.published["seconds"];
-  });
-
-  const newsItemsImagesUrls = newsItemImages.map((newsItemImage) => {
-    return newsItemImage.url;
-  });
-
-  console.log(newsItemsImagesUrls);
-
+  
   return (
     <div className="page">
       <h1>In The News</h1>
@@ -50,7 +26,7 @@ export default function InTheNews() {
           return (
             <div className="newsitem">
               <img
-                src={newsItemsImagesUrls[newsItems.indexOf(newsItem)]}
+                src={newsItem.imageurl}
                 alt="News Logo"
                 className="newslogo"
               />
