04c86611641df2c593a67f3090aa72d7d61d8994
Sean Mabli <smdrone1@gmail.com>
2022-09-23 23:40:39 -0400

    small stuff to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,14 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase/firebase";
 import { Divider } from "@mui/material";
 
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+
 import "./footer.css";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 export function Footer() {
   const [responses, setResponses] = useState([]);
@@ -20,12 +27,85 @@ export function Footer() {
     return first.name.localeCompare(second.name);
   });
 
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
+
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+
+  function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(
+      getWindowDimensions()
+    );
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowDimensions;
+  }
+
+  function Testing() {
+    const { height, width } = useWindowDimensions();
+
+     return (
+        <>
+          {responses.map((response, index) => (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <div className="sponsor">
+                  <a href={response.url}>
+                    <img
+                      src={response.imageurl}
+                      alt={response.name}
+                      className="sponsorimage"
+                    />
+                  </a>
+                </div>
+              ) : null}
+            </div>
+          ))}
+        </>
+      );
+  }
+
   return (
     <>
       <br />
       <Divider />
       <br />
       <p>Special Thanks To Our Sponsors:</p>
+      <br />
+      <AutoPlaySwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        <Testing />
+      </AutoPlaySwipeableViews>
       <br />
       <div className="sponsorscontainer">
         {responses.map((response) => {
@@ -41,6 +121,23 @@ export function Footer() {
             </div>
           );
         })}
+      </div>
+      <div>
+        <div>
+          <h2>Contact Us</h2>
+          <p>617-967-7755</p>
+          <p>info@smilemass.org</p>
+        </div>
+        <div>
+          <h2>Connect with us</h2>
+          <a href="https://www.facebook.com/SmileMass">Facebook</a>
+          <br />
+          <a href="https://www.instagram.com/smilemassorg/">Instagram</a>
+          <br />
+          <a href="https://twitter.com/smilemassorg">Twitter</a>
+          <br />
+          <a href="https://www.linkedin.com/company/smile-mass/">LinkedIn</a>
+        </div>
       </div>
     </>
   );
